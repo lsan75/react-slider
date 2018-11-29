fba9c86f017286cd824020b1dbce70c719503885
Laurent Sanchez <laurentsanchez@macbook-pro-de-laurent-2.home>
2018-11-29 20:24:35 +0100

    init slider

--- src/App.js
+++ src/App.js
@@ -1,27 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Swiper from 'swiper/dist/js/swiper'
+import '../node_modules/swiper/dist/css/swiper.css'
+import './App.css'
+
+import image1 from './monimage1.jpg'
+import image2 from './monimage2.jpg'
+
+const myChildren = () => {
+  const parentNodes = [1, 2, 3]
+  const childNodes = [0, 1]
+  const images = [ image1, image2 ]
+
+  return parentNodes.map(item => (
+    <div className="swiper-slide" key={item}>
+      <a href="http://google.fr">
+        <div className="child swiper-container">
+          <div className="swiper-wrapper">
+            {childNodes.map(sub => <article className="swiper-slide" key={sub}>
+              <img src={images[sub]} className="swiper-lazy" />
+            </article>)}
+          </div>
+          <aside className="swiper-button-prev" />
+          <aside className="swiper-button-next" />
+        </div>
+      </a>
+    </div>
+  ))
+}
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      parent: null,
+      child: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      parent: new Swiper('.parent', {
+        loop: false,
+        allowTouchMove: false
+      }),
+      child: new Swiper('.child', {
+        loop: true,
+        initialSlide: 0,
+        grabCursor: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        lazy: {
+          loadPrevNext: true,
+        },
+      })
+    })
+  }
+
+  prev() {
+    this.state.parent.slidePrev()
+  }
+  next() {
+    this.state.parent.slideNext()
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <React.Fragment>
+        <div className="parent swiper-container">
+          <div className="swiper-wrapper">
+            {myChildren()}
+          </div>
+        </div>
+        <div onClick={() => this.next()}>next</div>
+        <div onClick={() => this.prev()}>prev</div>
+      </React.Fragment>
+    )
   }
 }
 
